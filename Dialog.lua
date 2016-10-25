847461e4b4e6891c8f15f91acaafa2dc22a0fa68
Bernhard Saumweber <b.saumweber@gmail.com>
2016-10-25 20:03:22 +0200

    Adds Premade Groups Filter version 1.0 supporting English language

---
+++ Dialog.lua
@@ -0,0 +1,286 @@
+-------------------------------------------------------------------------------
+-- Premade Groups Filter
+-------------------------------------------------------------------------------
+-- Copyright (C) 2015 Elotheon-Arthas-EU
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+-------------------------------------------------------------------------------
+
+local PGF = PremadeGroupsFilter
+local L = select(2, ...)
+
+function PGF.GameTooltip_AddWhite(left)
+    GameTooltip:AddLine(left, 255, 255, 255)
+end
+
+function PGF.GameTooltip_AddDoubleWhite(left, right)
+    GameTooltip:AddDoubleLine(left, right, 255, 255, 255, 255, 255, 255)
+end
+
+function PGF.Dialog_InfoButton_OnEnter(self, motion)
+    local AddDoubleWhiteUsingKey = function (key)
+        PGF.GameTooltip_AddDoubleWhite(key, L["dialog.tooltip." .. key]) end
+
+    GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+    GameTooltip:SetText(L["dialog.tooltip.title"])
+    GameTooltip:AddLine(" ")
+    GameTooltip:AddDoubleLine(L["dialog.tooltip.variable"], L["dialog.tooltip.description"])
+    AddDoubleWhiteUsingKey("name")
+    AddDoubleWhiteUsingKey("comment")
+    AddDoubleWhiteUsingKey("ilvl")
+    AddDoubleWhiteUsingKey("hlvl")
+    AddDoubleWhiteUsingKey("defeated")
+    AddDoubleWhiteUsingKey("members")
+    AddDoubleWhiteUsingKey("tanks")
+    AddDoubleWhiteUsingKey("heals")
+    AddDoubleWhiteUsingKey("dps")
+    AddDoubleWhiteUsingKey("age")
+    AddDoubleWhiteUsingKey("voice")
+    AddDoubleWhiteUsingKey("myrealm")
+    PGF.GameTooltip_AddDoubleWhite("hm/brf/hfc/en/nh", L["dialog.tooltip.raids"])
+    GameTooltip:AddLine(" ")
+    GameTooltip:AddDoubleLine(L["dialog.tooltip.op.logic"], L["dialog.tooltip.example"])
+    PGF.GameTooltip_AddDoubleWhite("()", L["dialog.tooltip.ex.parentheses"])
+    PGF.GameTooltip_AddDoubleWhite("not", L["dialog.tooltip.ex.not"])
+    PGF.GameTooltip_AddDoubleWhite("and", L["dialog.tooltip.ex.and"])
+    PGF.GameTooltip_AddDoubleWhite("or", L["dialog.tooltip.ex.or"])
+    GameTooltip:AddLine(" ")
+    GameTooltip:AddDoubleLine(L["dialog.tooltip.op.number"], L["dialog.tooltip.example"])
+    PGF.GameTooltip_AddDoubleWhite("==", L["dialog.tooltip.ex.eq"])
+    PGF.GameTooltip_AddDoubleWhite("~=", L["dialog.tooltip.ex.neq"])
+    PGF.GameTooltip_AddDoubleWhite("<,>,<=,>=", L["dialog.tooltip.ex.lt"])
+    GameTooltip:AddLine(" ")
+    GameTooltip:AddDoubleLine(L["dialog.tooltip.op.string"], L["dialog.tooltip.example"])
+    PGF.GameTooltip_AddDoubleWhite("name:find(\"x\")", L["dialog.tooltip.ex.find"])
+    PGF.GameTooltip_AddDoubleWhite("name:match(\"x\")", L["dialog.tooltip.ex.match"])
+    GameTooltip:Show()
+end
+
+function PGF.Dialog_InfoButton_OnLeave(self, motion)
+    GameTooltip:Hide()
+end
+
+function PGF.Dialog_OnModelUpdate()
+    local exp = PGF.GetExpressionFromModel()
+    if PGF.Empty(exp) or exp == "true" then exp = "" end
+    exp = exp:gsub("^true and ", "")
+    PremadeGroupsFilterDialog.Expression.EditBox.Instructions:SetText(exp)
+end
+
+function PGF.Dialog_Act_OnClick(self, button, down)
+    local dialog = PremadeGroupsFilterDialog
+    local key = self:GetParent():GetAttribute("parentKey")
+    local checked = self:GetChecked()
+    PGF.model[key:lower()].act = checked
+    if key == "Ilvl" then
+        dialog.Noilvl.Act:SetEnabled(checked)
+        if not checked then
+            dialog.Noilvl.Act:SetChecked(false)
+            PGF.Dialog_Act_OnClick(dialog.Noilvl.Act)
+        end
+    end
+    PGF.Dialog_OnModelUpdate()
+    PGF.DebugPrint("state of " .. key .. " changed to " .. tostring(checked))
+end
+
+function PGF.Dialog_SetCheckbox(self, key, state)
+    self[key].Act:SetChecked(state)
+    PGF.Dialog_Act_OnClick(self[key].Act)
+end
+
+function PGF.Dialog_ToggleCheckboxAccordingToMinMaxFields(key)
+    local self = PremadeGroupsFilterDialog
+    local state = PGF.NotEmpty(self[key].Min:GetText()) or PGF.NotEmpty(self[key].Max:GetText())
+    PGF.Dialog_SetCheckbox(self, key, state)
+end
+
+function PGF.Dialog_ResetGenericField(self, key)
+    PGF.Dialog_SetCheckbox(self, key, false)
+end
+
+function PGF.Dialog_MinMax_OnTextChanged(self, userInput)
+    local selfKey = self:GetAttribute("parentKey")
+    local parentKey = self:GetParent():GetAttribute("parentKey")
+    local val = self:GetText()
+    PGF.model[parentKey:lower()][selfKey:lower()] = val
+    PGF.Dialog_ToggleCheckboxAccordingToMinMaxFields(parentKey)
+    --PGF.Dialog_OnModelUpdate() -- line above does that
+    PGF.DebugPrint(selfKey .. " of " .. parentKey .. " changed to " .. tostring(val))
+end
+
+function PGF.Dialog_Expression_OnTextChanged(self, userInput)
+    -- we cannot set the OnTextChange directly, since the InputScrollFrameTemplate
+    -- needs that for hiding/showing the gray instructions text
+    if self == PremadeGroupsFilterDialog.Expression.EditBox then
+        PGF.model.expression = self:GetText() or ""
+        PGF.Dialog_OnModelUpdate()
+    end
+end
+
+function PGF.Dialog_ResetMinMaxField(self, key)
+    PGF.Dialog_ResetGenericField(self, key)
+    self[key].Min:SetText("")
+    self[key].Max:SetText("")
+    PGF.Dialog_MinMax_OnTextChanged(self[key].Min)
+    PGF.Dialog_MinMax_OnTextChanged(self[key].Max)
+end
+
+function PGF:Dialog_RefreshButton_OnClick(self, button, down)
+    PGF.DebugPrint("refresh clicked")
+    LFGListSearchPanel_DoSearch(LFGListFrame.SearchPanel)
+end
+
+function PGF:Dialog_ResetButton_OnClick(self, button, down)
+    PGF.DebugPrint("reset clicked")
+    local dialog = PremadeGroupsFilterDialog
+    -- TODO: reset the difficulty dropdown
+    PGF.Dialog_ResetGenericField(dialog, "Difficulty")
+    PGF.Dialog_ResetMinMaxField(dialog, "Ilvl")
+    PGF.Dialog_ResetGenericField(dialog, "Noilvl")
+    PGF.Dialog_ResetMinMaxField(dialog, "Members")
+    PGF.Dialog_ResetMinMaxField(dialog, "Tanks")
+    PGF.Dialog_ResetMinMaxField(dialog, "Heals")
+    PGF.Dialog_ResetMinMaxField(dialog, "Dps")
+    PGF.Dialog_ResetMinMaxField(dialog, "Defeated")
+    dialog.Expression.EditBox:SetText("")
+    PGF.Dialog_Expression_OnTextChanged(dialog.Expression.EditBox)
+    PGF:Dialog_RefreshButton_OnClick(dialog.RefreshButton)
+end
+
+function PGF.Dialog_DifficultyDropdown_OnClick(item, dropdown, text)
+    --local dropdown = D.Difficulty.DropDown
+    if item.value then
+        PGF.model.difficulty.val = item.value
+        UIDropDownMenu_SetSelectedValue(dropdown, item.value)
+        UIDropDownMenu_SetText(dropdown, text)
+        PGF.Dialog_OnModelUpdate()
+    end
+end
+
+function PGF.Dialog_DifficultyDropdown_AddItem(dropdown, difficulty, text)
+    local info = UIDropDownMenu_CreateInfo()
+    info.value = difficulty
+    info.checked = PGF.model.difficulty.val == difficulty
+    info.func = PGF.Dialog_DifficultyDropdown_OnClick
+    info.arg1 = dropdown
+    info.arg2 = text
+    info.text = text
+    UIDropDownMenu_AddButton(info)
+end
+
+function PGF.Dialog_DifficultyDropdown_Init(self, level)
+    PGF.Dialog_DifficultyDropdown_AddItem(self, PGF.CONST.NORMAL, L["dialog.normal"])
+    PGF.Dialog_DifficultyDropdown_AddItem(self, PGF.CONST.HEROIC, L["dialog.heroic"])
+    PGF.Dialog_DifficultyDropdown_AddItem(self, PGF.CONST.MYTHIC, L["dialog.mythic"])
+end
+
+function PGF.Dialog_Min_OnTabPressed(self)
+    self:GetParent().Max:SetFocus()
+end
+
+function PGF.Dialog_Max_OnTabPressed(self)
+    self:GetParent().Min:SetFocus()
+    -- TODO switch to next editbox instead
+end
+
+function PGF.Dialog_SetUpGenericField(self, key)
+    self[key]:SetAttribute("parentKey", key)
+    self[key].Title:SetText(L["dialog." .. key:lower()])
+    self[key].Act:SetScript("OnClick", PGF.Dialog_Act_OnClick)
+end
+
+function PGF.Dialog_SetUpMinMaxField(self, key)
+    PGF.Dialog_SetUpGenericField(self, key)
+    self[key].Min:SetText(PGF.model[key:lower()].min)
+    self[key].Min:SetAttribute("parentKey", "Min")
+    self[key].Max:SetText(PGF.model[key:lower()].max)
+    self[key].Max:SetAttribute("parentKey", "Max")
+    self[key].To:SetText(L["dialog.to"])
+    self[key].Min:SetScript("OnTextChanged", PGF.Dialog_MinMax_OnTextChanged)
+    self[key].Max:SetScript("OnTextChanged", PGF.Dialog_MinMax_OnTextChanged)
+    self[key].Min:SetScript("OnTabPressed", PGF.Dialog_Min_OnTabPressed)
+    self[key].Max:SetScript("OnTabPressed", PGF.Dialog_Max_OnTabPressed)
+end
+
+function PGF.Dialog_OnLoad()
+    local dialog = PremadeGroupsFilterDialog
+
+    dialog.InsetBg:SetPoint("TOPLEFT", 4, -62)
+    dialog.InsetBg:SetPoint("BOTTOMRIGHT", -6, 26)
+    dialog.Title:SetText("Premade Groups Filter")
+    dialog.ResetButton:SetText(L["dialog.reset"])
+    dialog.ResetButton:SetScript("OnClick", PGF.Dialog_ResetButton_OnClick)
+    dialog.RefreshButton:SetText(L["dialog.refresh"])
+    dialog.RefreshButton:SetScript("OnClick", PGF.Dialog_RefreshButton_OnClick)
+    dialog.SimpleExplanation:SetText(L["dialog.expl.simple"])
+    dialog.StateExplanation:SetText(L["dialog.expl.state"])
+    dialog.MinExplanation:SetText(L["dialog.expl.min"])
+    dialog.MaxExplanation:SetText(L["dialog.expl.max"])
+    dialog.Advanced.Explanation:SetText(L["dialog.expl.advanced"])
+    dialog.Advanced.InfoButton:SetScript("OnEnter", PGF.Dialog_InfoButton_OnEnter)
+    dialog.Advanced.InfoButton:SetScript("OnLeave", PGF.Dialog_InfoButton_OnLeave)
+    dialog.Ilvl.Min:SetMaxLetters(3)
+    dialog.Ilvl.Max:SetMaxLetters(3)
+    dialog.Noilvl.Title:SetWidth(210)
+    dialog.Noilvl.Act:SetEnabled(false)
+    dialog.Defeated.Title:SetWordWrap(true)
+    dialog.Defeated.Title:SetHeight(28)
+
+    PGF.Dialog_SetUpGenericField(dialog, "Difficulty")
+    PGF.Dialog_SetUpMinMaxField(dialog, "Ilvl")
+    PGF.Dialog_SetUpGenericField(dialog, "Noilvl")
+    PGF.Dialog_SetUpMinMaxField(dialog, "Members")
+    PGF.Dialog_SetUpMinMaxField(dialog, "Tanks")
+    PGF.Dialog_SetUpMinMaxField(dialog, "Heals")
+    PGF.Dialog_SetUpMinMaxField(dialog, "Dps")
+    PGF.Dialog_SetUpMinMaxField(dialog, "Defeated")
+
+    local font = dialog.SimpleExplanation:GetFont()
+    dialog.Expression.EditBox:SetFont(font, PGF.CONST.FONTSIZE_TEXTBOX)
+    dialog.Expression.EditBox.Instructions:SetFont(font, PGF.CONST.FONTSIZE_TEXTBOX)
+
+    UIDropDownMenu_Initialize(dialog.Difficulty.DropDown, PGF.Dialog_DifficultyDropdown_Init)
+    UIDropDownMenu_SetText(dialog.Difficulty.DropDown, L["dialog.mythic"])
+    UIDropDownMenu_SetWidth(dialog.Difficulty.DropDown, 90)
+    PGF.DebugPrint("dialog onload completed")
+end
+
+function PGF.Dialog_Toggle()
+    local dialog = PremadeGroupsFilterDialog
+    if PVEFrame:IsVisible()
+            and GroupFinderFrame.selection == LFGListPVEStub
+            and LFGListFrame.activePanel == LFGListFrame.SearchPanel then
+        dialog:Show()
+    else
+        dialog:Hide()
+    end
+end
+
+function PGF.Dialog_UpdatePosition()
+    local dialog = PremadeGroupsFilterDialog
+    dialog:SetPoint("TOPLEFT", GroupFinderFrame, "TOPRIGHT")
+    dialog:SetPoint("BOTTOMLEFT", GroupFinderFrame, "BOTTOMRIGHT")
+    dialog:SetWidth(300)
+end
+
+function PGF.Dialog_OnShow(dialog)
+    PGF.Dialog_UpdatePosition(dialog)
+end
+
+hooksecurefunc("LFGListFrame_SetActivePanel", PGF.Dialog_Toggle)
+hooksecurefunc("GroupFinderFrame_ShowGroupFrame", PGF.Dialog_Toggle)
+hooksecurefunc("InputScrollFrame_OnTextChanged", PGF.Dialog_Expression_OnTextChanged)
+PVEFrame:SetScript("OnShow", PGF.Dialog_Toggle)
+PVEFrame:SetScript("OnHide", PGF.Dialog_Toggle)
---
+++ Main.lua
@@ -0,0 +1,322 @@
+-------------------------------------------------------------------------------
+-- Premade Groups Filter
+-------------------------------------------------------------------------------
+-- Copyright (C) 2015 Elotheon-Arthas-EU
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+-------------------------------------------------------------------------------
+
+PremadeGroupsFilter = {}
+PremadeGroupsFilterDB = PremadeGroupsFilterDB or {}
+
+local PGF = PremadeGroupsFilter
+local L = select(2, ...)
+
+PGF.CONST = {
+    DEBUG = false,
+    NORMAL = 1,
+    HEROIC = 2,
+    MYTHIC = 3,
+    DEFAULT = {
+        DIFFICULTY = 3, -- MYTHIC, may not yet use constant here
+        ILVL       = { MIN = 800, MAX = 850 },
+        MEMBERS    = { MIN =   0, MAX =  40 },
+        TANKS      = { MIN =   0, MAX =   2 },
+        HEALS      = { MIN =   0, MAX =   8 },
+        DPS        = { MIN =   0, MAX =  30 },
+        DEFEATED   = { MIN =   0, MAX =  15 },
+    },
+    FONTSIZE_TEXTBOX = 12,
+}
+
+PGF.CONST.DIFFICULTY_STRING = {
+    [1] = "normal",
+    [2] = "heroic",
+    [3] = "mythic",
+}
+
+-- /run for i=455,480,1 do local n=C_LFGList.GetActivityInfo(i);print(i,n)end
+PGF.CONST.ACTIVITY_DIFFICULTY = {
+    -- Warlords of Draenor (raids only)
+    [37]  = PGF.CONST.NORMAL, -- Highmaul
+    [38]  = PGF.CONST.HEROIC, -- Highmaul
+    [399] = PGF.CONST.MYTHIC, -- Highmaul
+
+    [39]  = PGF.CONST.NORMAL, -- Blackrock Foundry
+    [40]  = PGF.CONST.HEROIC, -- Blackrock Foundry
+    [400] = PGF.CONST.MYTHIC, -- Blackrock Foundry
+
+    [409] = PGF.CONST.NORMAL, -- Hellfire Citadel
+    [410] = PGF.CONST.HEROIC, -- Hellfire Citadel
+    [412] = PGF.CONST.MYTHIC, -- Hellfire Citadel
+
+    -- Legion
+    [413] = PGF.CONST.NORMAL, -- The Emerald Nightmare
+    [414] = PGF.CONST.HEROIC, -- The Emerald Nightmare
+    [415] = PGF.CONST.NORMAL, -- The Nighthold
+    [416] = PGF.CONST.HEROIC, -- The Nighthold
+
+    [417] = PGF.CONST.NORMAL, -- Random Dungeon
+    [418] = PGF.CONST.HEROIC, -- Random Dungeon
+
+    [425] = PGF.CONST.NORMAL, -- Eye of Azshara
+    [426] = PGF.CONST.NORMAL, -- Darkheart Thicket
+    [427] = PGF.CONST.NORMAL, -- Halls of Valor
+    [428] = PGF.CONST.NORMAL, -- Neltharion's Lair
+    [429] = PGF.CONST.NORMAL, -- Violet Hold
+    [430] = PGF.CONST.NORMAL, -- Black Rook Hold
+    [431] = PGF.CONST.NORMAL, -- Vault of the Wardens
+    [432] = PGF.CONST.NORMAL, -- Maw of Souls
+    [433] = PGF.CONST.NORMAL, -- Court of Stars
+    [434] = PGF.CONST.NORMAL, -- The Arcway
+
+    [435] = PGF.CONST.HEROIC, -- Eye of Azshara
+    [436] = PGF.CONST.HEROIC, -- Darkheart Thicket
+    [437] = PGF.CONST.HEROIC, -- Halls of Valor
+    [438] = PGF.CONST.HEROIC, -- Neltharion's Lair
+    [439] = PGF.CONST.HEROIC, -- Violet Hold
+    [440] = PGF.CONST.HEROIC, -- Black Rook Hold
+    [441] = PGF.CONST.HEROIC, -- Vault of the Wardens
+    [442] = PGF.CONST.HEROIC, -- Maw of Souls
+    [443] = PGF.CONST.HEROIC, -- Court of Stars
+    [444] = PGF.CONST.HEROIC, -- The Arcway
+
+    [445] = PGF.CONST.MYTHIC, -- Eye of Azshara
+    [446] = PGF.CONST.MYTHIC, -- Darkheart Thicket
+    [447] = PGF.CONST.MYTHIC, -- Halls of Valor
+    [448] = PGF.CONST.MYTHIC, -- Neltharion's Lair
+    [449] = PGF.CONST.MYTHIC, -- Violet Hold
+    [450] = PGF.CONST.MYTHIC, -- Black Rook Hold
+    [451] = PGF.CONST.MYTHIC, -- Vault of the Wardens
+    [452] = PGF.CONST.MYTHIC, -- Maw of Souls
+    [453] = PGF.CONST.MYTHIC, -- Court of Stars
+    [454] = PGF.CONST.MYTHIC, -- The Arcway
+}
+
+PGF.model = {
+    expression = "",
+    difficulty = {
+        act = false,
+        val = PGF.CONST.DEFAULT.DIFFICULTY,
+    },
+    ilvl = {
+        act = false,
+        min = "", --PGF.CONST.DEFAULT.ILVL.MIN,
+        max = "", --PGF.CONST.DEFAULT.ILVL.MAX,
+    },
+    noilvl = {
+        act = false
+    },
+    members = {
+        act = false,
+        min = "", --PGF.CONST.DEFAULT.MEMBERS.MIN,
+        max = "", --PGF.CONST.DEFAULT.MEMBERS.MAX,
+    },
+    tanks = {
+        act = false,
+        min = "", --PGF.CONST.DEFAULT.TANKS.MIN,
+        max = "", --PGF.CONST.DEFAULT.TANKS.MAX,
+    },
+    heals = {
+        act = false,
+        min = "", --PGF.CONST.DEFAULT.HEALS.MIN,
+        max = "", --PGF.CONST.DEFAULT.HEALS.MAX,
+    },
+    dps = {
+        act = false,
+        min = "", --PGF.CONST.DEFAULT.DPS.MIN,
+        max = "", --PGF.CONST.DEFAULT.DPS.MAX,
+    },
+    defeated = {
+        act = false,
+        min = "", --PGF.CONST.DEFAULT.DEFEATED.MIN,
+        max = "", --PGF.CONST.DEFAULT.DEFEATED.MAX,
+    },
+}
+
+StaticPopupDialogs["PEF_ERRORPOPUP"] = {
+    text = "%s",
+    button1 = L["button.ok"],
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3,
+}
+
+function PGF.DebugPrint(value)
+    if PGF.CONST.DEBUG then print("[PGF][Debug] " .. value) end
+end
+
+function PGF.NotEmpty(value) return value and value ~= "" end
+function PGF.Empty(value) return not PGF.NotEmpty(value) end
+
+function PGF.HandleSyntaxError(error)
+    StaticPopup_Show("PEF_ERRORPOPUP", string.format(L["error.syntax"], error))
+end
+
+function PGF.HandleSemanticError(error)
+    StaticPopup_Show("PEF_ERRORPOPUP", string.format(L["error.semantic"], error))
+end
+
+PGF.filterMetaTable = {
+    __mode = "k",
+    __index = function(table, key)
+        local func, error = loadstring("return " .. key)
+        if error then
+            PGF.HandleSyntaxError(error)
+            return nil
+        end
+        table[key] = func
+        return func
+    end,
+    tonumber = tonumber
+}
+
+PGF.filter = setmetatable({}, PGF.filterMetaTable)
+
+function PGF.GetExpressionFromMinMaxModel(key)
+    local exp = ""
+    if PGF.model[key].act then
+        if PGF.NotEmpty(PGF.model[key].min) then exp = exp .. " and " .. key .. ">=" .. PGF.model[key].min end
+        if PGF.NotEmpty(PGF.model[key].max) then exp = exp .. " and " .. key .. "<=" .. PGF.model[key].max end
+    end
+    return exp
+end
+
+function PGF.GetExpressionFromIlvlModel()
+    local exp = PGF.GetExpressionFromMinMaxModel("ilvl")
+    if PGF.model.noilvl.act and PGF.NotEmpty(exp) then
+        exp = exp:gsub("^ and ", "")
+        exp = " and (" .. exp .. " or ilvl==0)"
+    end
+    return exp
+end
+
+function PGF.GetExpressionFromDifficultyModel()
+    if PGF.model.difficulty.act then
+        return " and " .. PGF.CONST.DIFFICULTY_STRING[PGF.model.difficulty.val]
+    end
+    return ""
+end
+
+function PGF.GetExpressionFromAdvancedExpression()
+    if PGF.model.expression and PGF.model.expression ~= "" then
+        return " and " .. PGF.model.expression
+    end
+    return ""
+end
+
+function PGF.GetExpressionFromModel()
+    local exp = "true" -- start with neutral element
+    exp = exp .. PGF.GetExpressionFromDifficultyModel()
+    exp = exp .. PGF.GetExpressionFromIlvlModel()
+    exp = exp .. PGF.GetExpressionFromMinMaxModel("members")
+    exp = exp .. PGF.GetExpressionFromMinMaxModel("tanks")
+    exp = exp .. PGF.GetExpressionFromMinMaxModel("heals")
+    exp = exp .. PGF.GetExpressionFromMinMaxModel("dps")
+    exp = exp .. PGF.GetExpressionFromMinMaxModel("defeated")
+    exp = exp .. PGF.GetExpressionFromAdvancedExpression()
+    PGF.DebugPrint("Expression = " .. exp)
+    exp = exp:gsub("^true and ", "")
+    return exp
+end
+
+function PGF.DoesPassThroughFilter(env, exp)
+    --local exp = "mythic and tansk < 0 and members==4"  -- raises semantic error
+    --local exp = "and and tanks==0 and members==4"      -- raises syntax error
+    --local exp = "mythic and tanks==0 and members==4"   -- correct statement
+    local filter = PGF.filter[exp]
+    if filter then
+        setfenv(filter, env)
+        local hasFilterError, filterResult = pcall(filter)
+        if hasFilterError then
+            return filterResult
+        else
+            PGF.HandleSemanticError(filterResult)
+            return true
+        end
+    end
+    return true
+end
+
+function PGF.SortSearchResults(results)
+    local exp = PGF.GetExpressionFromModel()
+    if exp == "true" then return end -- skip trivial expression
+
+    -- loop backwards through the results list so we can remove elements from the table
+    for idx = #results, 1, -1 do
+        local resultID = results[idx]
+        local _, activityID, name, comment, voiceChat, iLvl, honorLevel, age,
+              numBNetFriends, numCharFriends, numGuildMates, _, leaderName,
+              numMembers = C_LFGList.GetSearchResultInfo(resultID)
+        local completedEncounters = C_LFGList.GetSearchResultEncounterInfo(resultID)
+        local memberCounts = C_LFGList.GetSearchResultMemberCounts(resultID)
+
+        local env = {}
+        env.activity = activityID
+        env.name = name:lower()
+        env.comment = comment:lower()
+        env.leader = leaderName and leaderName:lower()
+        env.age = math.floor(age / 60) -- age in minutes
+        env.voice = voiceChat and voiceChat ~= ""
+        env.ilvl = iLvl or 0
+        env.hlvl = honorLevel or 0
+        env.friends = numBNetFriends + numCharFriends + numGuildMates
+        env.members = numMembers
+        env.tanks = memberCounts.TANK
+        env.heals = memberCounts.HEALER
+        env.dps = memberCounts.DAMAGER + memberCounts.NOROLE
+        env.defeated = completedEncounters and #completedEncounters or 0
+        env.normal = PGF.CONST.ACTIVITY_DIFFICULTY[activityID] == PGF.CONST.NORMAL
+        env.heroic = PGF.CONST.ACTIVITY_DIFFICULTY[activityID] == PGF.CONST.HEROIC
+        env.mythic = PGF.CONST.ACTIVITY_DIFFICULTY[activityID] == PGF.CONST.MYTHIC
+        env.myrealm = leaderName and not leaderName:find('-')
+
+        env.hm  = activityID ==  37 or activityID ==  38 or activityID == 399
+        env.brf = activityID ==  39 or activityID ==  40 or activityID == 400
+        env.hfc = activityID == 409 or activityID == 410 or activityID == 412
+        env.en  = activityID == 413 or activityID == 414
+        env.nh  = activityID == 415 or activityID == 416
+
+        if not PGF.DoesPassThroughFilter(env, exp) then table.remove(results, idx) end
+    end
+end
+
+-- this function is an excact copy of Blizzard's implementation except the marked part in the middle
+function PGF.DoSearch(panel)
+    local searchText = panel.SearchBox:GetText();
+
+    -- modification begin
+    PGF.filterExpression = nil
+    local searchExpression, filterExpression = searchText:match("^([^=]-)=(.+)$")
+    if filterExpression then
+        PGF.filterExpression = filterExpression:lower()
+        searchText = searchExpression
+    end
+    -- modification end
+
+    local languages = C_LFGList.GetLanguageSearchFilter();
+    C_LFGList.Search(panel.categoryID, searchText, panel.filters, panel.preferredFilters, languages);
+    panel.searching = true;
+    panel.searchFailed = false;
+    panel.selectedResult = nil;
+    LFGListSearchPanel_UpdateResultList(panel);
+    LFGListSearchPanel_UpdateResults(panel);
+end
+
+hooksecurefunc("LFGListUtil_SortSearchResults", PGF.SortSearchResults)
+--hooksecurefunc("LFGListSearchPanel_DoSearch", PGF.DoSearch)
+
+LFGListFrame.SearchPanel.SearchBox:SetMaxLetters(2048)
