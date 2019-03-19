c9a3e219ae815951c81a9388807b4d95df3f6fb5
Bernhard Saumweber <b.saumweber@gmail.com>
2019-03-19 15:28:34 +0100

    Refactored code and added a new keyword 'declined' for filtering groups that have declined you in the last 10 minutes

--- Init.lua
+++ Init.lua
@@ -61,6 +61,7 @@ C.DIFFICULTY_STRING = {
 }
 
 C.COLOR_ENTRY_NEW       = { R = 0.3, G = 1.0, B = 0.3 } -- green
+C.COLOR_ENTRY_DECLINED  = { R = 0.5, G = 0.1, B = 0.1 } -- dark red
 C.COLOR_LOCKOUT_PARTIAL = { R = 1.0, G = 0.5, B = 0.1 } -- orange
 C.COLOR_LOCKOUT_FULL    = { R = 0.5, G = 0.1, B = 0.1 } -- red
 C.COLOR_LOCKOUT_MATCH   = { R = 1.0, G = 1.0, B = 1.0 } -- white
