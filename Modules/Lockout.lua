0ca81c4489782a1284e5b4df26623b592a84d195
Bernhard Saumweber <b.saumweber@gmail.com>
2020-02-09 11:40:45 +0100

    Update copyright header to year 2020

--- Modules/Lockout.lua
+++ Modules/Lockout.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
 -- Premade Groups Filter
 -------------------------------------------------------------------------------
--- Copyright (C) 2015 Elotheon-Arthas-EU
+-- Copyright (C) 2020 Elotheon-Arthas-EU
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
