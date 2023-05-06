cbbd2d61aab2add59e44b5c59f8591d3d3ae97ef
Bernhard Saumweber <b.saumweber@gmail.com>
2023-05-06 22:34:46 +0200

    Add optional second parameter to macro function for sorting expression (fixes #165)

--- Modules/Macro.lua
+++ Modules/Macro.lua
@@ -22,12 +22,16 @@ local PGF = select(2, ...)
 local L = PGF.L
 local C = PGF.C
 
-function PGF.Macro(exp)
+function PGF.Macro(exp, sorting)
     local dialog = PremadeGroupsFilterDialog
     if dialog and dialog:IsVisible() then
         PGF.Dialog_Reset()
         dialog.Expression.EditBox:SetText(exp)
         PGF.Dialog_Expression_OnTextChanged(dialog.Expression.EditBox)
+        if sorting then
+            dialog.Sorting.SortingExpression:SetText(sorting)
+            PGF.Dialog_SortingExpression_OnTextChanged(dialog.Sorting.SortingExpression)
+        end
         dialog.RefreshButton:Click()
     end
 end
