cf9d1ab57f0fa83872d03ca0c4655898978dca6f
Bernhard Saumweber <b.saumweber@gmail.com>
2024-04-24 09:47:05 +0200

    Update copyright to 2024

--- Localization/itIT.lua
+++ Localization/itIT.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
 -- Premade Groups Filter
 -------------------------------------------------------------------------------
--- Copyright (C) 2022 Elotheon-Arthas-EU
+-- Copyright (C) 2024 Bernhard Saumweber
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
