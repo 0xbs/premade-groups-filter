fc78932a5f3e391c5b66a8d1572f33f56a64fd21
Bernhard Saumweber <b.saumweber@gmail.com>
2024-05-06 09:48:54 +0200

    Add missing header for Cata version file (fixes #251)

--- Modules/Activity_Cata.lua
+++ Modules/Activity_Cata.lua
@@ -1,3 +1,27 @@
+-------------------------------------------------------------------------------
+-- Premade Groups Filter
+-------------------------------------------------------------------------------
+-- Copyright (C) 2024 Bernhard Saumweber
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+-------------------------------------------------------------------------------
+
+local PGF = select(2, ...)
+local L = PGF.L
+local C = PGF.C
+
 C.ACTIVITY = {
     [ 796] = { difficulty = 1, category =   2, mapID =   43 }, -- Wailing Caverns
     [ 797] = { difficulty = 1, category =   2, mapID =  289 }, -- Scholomance
