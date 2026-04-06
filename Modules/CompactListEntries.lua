3c889ebc74946feacffc078d0b940eb5d9dfb283
Bernhard Saumweber <b.saumweber@gmail.com>
2026-04-06 11:47:27 +0200

    Allow compactListEntries even while restricted

--- Modules/CompactListEntries.lua
+++ Modules/CompactListEntries.lua
@@ -55,7 +55,7 @@ end
 
 function PGF.InitCompactListEntries()
     if not PremadeGroupsFilterSettings.compactListEntries then return end
-    if PGF.IsRestricted() then return end
+    --if PGF.IsRestricted() then return end
 
     hooksecurefunc("LFGListSearchEntry_Update", PGF.CompactListEntries_UpdateListEntry)
     PVEFrame:HookScript("OnShow", function () PGF.CompactListEntries_UpdateListScrollBox() end)
